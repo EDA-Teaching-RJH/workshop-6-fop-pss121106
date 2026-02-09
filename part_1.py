8436ab6aa983dba1ef14f7c63b969fcc3aab23cd
pss121106 <pss1@kent.ac.uk>
2026-02-09 11:31:03 +0000

    Complete Part 1

--- part_1.py
+++ part_1.py
@@ -1,1 +1,15 @@
+sample_bay = ["Basalt", "Silica", "Iron", "Dust"]
 
+print(sample_bay[0])
+print(sample_bay[-1])
+print(len(sample_bay))
+
+for sample in sample_bay:
+    print(f"Transmitting data for {sample}")
+
+new_findings = []
+
+for i in range(3):
+    user_input = input("Enter a rock type to add to the findings:")
+    new_findings.append(user_input)
+    print("Find added successfully. current_findings: ", new_findings)
