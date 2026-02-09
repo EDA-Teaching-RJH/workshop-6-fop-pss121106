28963fd5d5dce5d05efb50832f74079f55ef12d9
pss121106 <pss1@kent.ac.uk>
2026-02-09 12:29:06 +0000

    Part 2 Complete

--- part_2.py
+++ part_2.py
@@ -1,1 +1,17 @@
+rover_status = ["Battery: 100%", "Heater: Off", "Camera: On"]
+current_status = rover_status[0]
+print(current_status)
 
+print("The rover is moving and conditions are changing.")
+rover_status[0] = "Battery: 85%"
+print(rover_status[0])
+rover_status[1] = "Heater: On"
+print(rover_status[1])
+rover_status[2] = "Camera: Off"
+rover_status.append("Speed: 5")
+print("Here is the updated rover status:" , current_status)
+
+mission_log = [{"site": "Site Crater A", "radiator": "Radiator: Low", "water": "Water: False"}, {"site": "Site B", "radiator": "Radiator: High", "water": "Water: True"}]
+
+for site in mission_log:
+    print(f"Site: {site['site']}, Radiation: {site['radiator']}")
